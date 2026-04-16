e2640ec3dff35b5de5e95e681cc27ed70cd00c1c
2017000015JuanEscobar <j.escobar.5ddb@gmail.com>
2026-04-15 21:46:07 -0600

    Add files via upload

---
+++ GA3_U2_JuanEscobar_IVD_Clave8/src/main/java/com/mycompany/ga3_u2_juanescobar_ivd_clave8/GA3_U2_JuanEscobar_IVD_Clave8.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ga2_u2_juanescobar_ivd_clave8;
+
+/**
+ *
+ * @author ladpa
+ */
+public class GA3_U2_JuanEscobar_IVD_Clave8 {
+
+    public static void main(String[] args) {
+        jfrmMensajes pedri = new jfrmMensajes();
+        pedri.setVisible(true);
+    }
+}
